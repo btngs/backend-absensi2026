1415cc485ddf4c9c633fd0dc26e317410d811f25
Bintang Syahri <anjayanto08@gmail.com>
2026-08-06 14:20:00 +0700

    auth routing

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUser);
-router.post('/', userController.createNewUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.post('/create', userController.createNewUser);
+router.put('/update/:id', userController.updateUser);
+router.delete('/delete/:id', userController.deleteUser);
 
 module.exports = router;
