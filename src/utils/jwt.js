9306355acc88b56563193ba98bc057cefa93cbcd
Bintang Syahri <anjayanto08@gmail.com>
2026-08-06 14:21:27 +0700

    jwt and hashing password

---
+++ src/utils/jwt.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
+const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+const comparePassword = async (password, hashedPassword) => {
+    return await bcrypt.compare(password, hashedPassword);
+}
+
+const generateAccessToken = (payLoad) => {
+    return jwt.sign(payLoad, ACCESS_SECRET, { expiresIn: '30m' });
+}
+
+const generateRefreshToken = (payLoad) => {
+    return jwt.sign(payLoad, REFRESH_SECRET, { expiresIn: '7d' });
+}
+
+module.exports = {
+    hashPassword,
+    comparePassword,
+    generateAccessToken,
+    generateRefreshToken
+};
