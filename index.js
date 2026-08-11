7cb274a99319f7b804b3064360eeabcab1b4e8b3
Bintang Syahri <anjayanto08@gmail.com>
2026-08-11 13:54:48 +0700

    adding documentation in swagger

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./src/docs/swagger');
 
 //routes
@@ -28,7 +27,42 @@ app.use(cors(corsOptions));
 app.use(middlewareLog.logRequest);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/api-docs.json', (req, res) => {
+    res.json(swaggerDocument);
+});
+
+app.get('/api-docs', (req, res) => {
+    res.type('html').send(`<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Attendance Backend API Docs</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+    <style>
+      html, body { margin: 0; padding: 0; height: 100%; }
+      #swagger-ui { min-height: 100%; }
+    </style>
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js" crossorigin></script>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js" crossorigin></script>
+    <script>
+      window.onload = function () {
+        window.ui = SwaggerUIBundle({
+          url: '/api-docs.json',
+          dom_id: '#swagger-ui',
+          deepLinking: true,
+          presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+          layout: 'BaseLayout'
+        });
+      };
+    </script>
+  </body>
+</html>`);
+});
 
 app.use('/users', usersRoutes);
 app.use('/auth', authRoutes);
