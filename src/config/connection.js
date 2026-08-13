92ef6cab8b93ba8e9e08e9262d7b501a1d22a087
Bintang Syahri <anjayanto08@gmail.com>
2026-08-13 10:49:05 +0700

    wkwk

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-console.log({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-});
+// console.log({
+//   host: process.env.DB_HOST,
+//   port: process.env.PORT,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+// });
 
 const conn = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: Number(process.env.PORT),
+    port: Number(process.env.DB_PORT),
     ssl: {
         rejectUnauthorized: false
     },
